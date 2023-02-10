a985146b4cec85861a45f12673269f83bfbb6fd1
Anton Sukhonosenko <anton.sukhonosenko@jetbrains.com>
2023-02-10 21:21:12 +0100

    Disable preload

--- client/src/app/Main.tsx
+++ client/src/app/Main.tsx
@@ -11,14 +11,14 @@ function createPreloadLink(href: string, as: string = "image") {
     document.head.appendChild(link)
 }
 
-valentineTypes.forEach(type => {
-    createPreloadLink(require(`./../../resources/valentines/${type.name}-fullscreen.png`))
-    createPreloadLink(require(`./../../resources/valentines/${type.name}-sticker.png`))
-    createPreloadLink(require(`./../../resources/valentines/${type.name}-sticker-new.png`))
-})
-
-createPreloadLink(require(`./../../resources/kotik.png`))
-createPreloadLink(require(`./../../resources/new.svg`))
-createPreloadLink(require(`./../../resources/font/font/fontello.woff2`), "font")
+// valentineTypes.forEach(type => {
+//     createPreloadLink(require(`./../../resources/valentines/${type.name}-fullscreen.png`))
+//     createPreloadLink(require(`./../../resources/valentines/${type.name}-sticker.png`))
+//     createPreloadLink(require(`./../../resources/valentines/${type.name}-sticker-new.png`))
+// })
+//
+// createPreloadLink(require(`./../../resources/kotik.png`))
+// createPreloadLink(require(`./../../resources/new.svg`))
+// createPreloadLink(require(`./../../resources/font/font/fontello.woff2`), "font")
 
 ReactDOM.render(<App />, document.getElementById('root'));
