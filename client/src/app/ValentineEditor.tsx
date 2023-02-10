4cbe4d5906db0cf4ac61c709d30f6d4edbf05afc
Anton Sukhonosenko <anton.sukhonosenko@jetbrains.com>
2023-02-10 20:46:59 +0100

    Center textarea content vertically

--- client/src/app/ValentineEditor.tsx
+++ client/src/app/ValentineEditor.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import {ValentineType} from "./ValentineType";
 import "./ValentineEditor.css"
+import {useEffect, useLayoutEffect, useRef} from "react";
 
 type Props = {
     type: ValentineType;
@@ -10,24 +11,42 @@ type Props = {
 
 export const ValentineEditor = ({type, message, setMessage}: Props) => {
     const src = require(`./../../resources/valentines/${type.name}-fullscreen.png`)
-
     const limit = 100
+    const textareaRef = useRef<HTMLTextAreaElement>()
+
+    // https://stackoverflow.com/a/24676492/2401247
+    useLayoutEffect(() => {
+        const element = textareaRef.current
+        element.style.height = "auto";
+        element.style.height = element.scrollHeight + "px";
+    }, [textareaRef.current, message])
 
     return (
         <div className="valentine-editor">
             <img src={src}/>
-            <textarea
-                placeholder="Add message here"
-                value={message}
-                onChange={(e) => setMessage(e.target.value.slice(0, limit))}
+            <div
+                className="textarea-container"
                 style={{
-                    color: type.textColor,
                     top: type.top + 'px',
                     left: type.left + 'px',
                     width: type.width + 'px',
                     height: type.height + 'px',
                 }}
-            />
+            >
+                <textarea
+                    ref={textareaRef}
+                    placeholder="Add message here"
+                    value={message}
+                    onChange={(e) =>
+                        setMessage(e.target.value.slice(0, limit).replace('\n', ''))
+                    }
+                    style={{
+                        color: type.textColor,
+                        width: type.width + 'px',
+                        maxHeight: type.height + 'px',
+                    }}
+                />
+            </div>
         </div>
     )
 }
